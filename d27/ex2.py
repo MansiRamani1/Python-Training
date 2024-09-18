7690829c2efd292f911924c415e0f3a37b80ab91
Mansi Ramani <ramanimansi9@gmail.com>
2024-09-18 10:26:38 +0530

    d30

--- d27/ex2.py
+++ d27/ex2.py
@@ -6,5 +6,27 @@
 # output:7
 # expl:since,7 is single element and more than n/2 times,so it is the majority element
 
+arr=[2,13]
+
+count_dict={}
+n = len(arr)
+count=0
+result=False
+
+for i in arr:
+    if i in count_dict:
+        count_dict[i]+=1
+    else:
+        count_dict[i]=1
+
+for key,value in count_dict.items():
+    if value>n//2:
+        count+=1
+        result=True
+        print(key)
+        break
+if result!=True:
+      print(-1)
+
 
 
---
+++ d30/task2.py
@@ -0,0 +1,29 @@
+# Example 1:
+
+# Input: s = "()"
+
+
+s="LVIII"
+
+st=[]
+
+for i in st:
+# Output: true
+
+# Example 2:
+
+# Input: s = "()[]{}"
+
+# Output: true
+
+# Example 3:
+
+# Input: s = "(]"
+
+# Output: false
+
+# Example 4:
+
+# Input: s = "([])"
+
+# Output: true
