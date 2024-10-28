850f4840f55da5b809d805d9b9d3b721cd910e83
Mansi Ramani <ramanimansi9@gmail.com>
2024-10-28 15:09:45 +0530

    taskAPI

--- taskAPI/main.py
+++ taskAPI/main.py
@@ -12,7 +12,7 @@ router=APIRouter()
 def item_list():
   data=list(db["item_master"].find())#data=list(item_collection.find())
   for item in data:
-        item["_id"] = str(item["_id"])#return data json format
+        item["_id"]=str(item["_id"])#return data json format
   return data
 
 
@@ -39,7 +39,7 @@ def item_create(item_data:dict):
 def user_list():
   data=list(user_collection.find())
   for item in data:
-        item["_id"] = str(item["_id"])
+        item["_id"]=str(item["_id"])
   return data
 
 
@@ -52,29 +52,12 @@ def user_create(user_data:dict):
        return {"status_code": 200,"id":str(resp.inserted_id)}
    except Exception as e:
        return HTTPException(status_code=500, detail=f"Some error occured {e}")
-   
-
-# @router.get("/item_user_map/")
-# def user_list_map():
-#   data=list(user_item_collection.find())
-#   for item in data:
-#         item["_id"] = str(item["_id"])
-#   return data
-
-
-# @router.post("/items_users")
-# def user_item(user_id:str,item_id):
-#    try:
-#        resp=user_item_collection.insert_one(user_id,item_id)
-#        return {"status_code": 200,"id":str(resp.inserted_id)}
-#    except Exception as e:
-#        return HTTPException(status_code=500, detail=f"Some error occured {e}")
-
 
 @router.post("/purchase_item")
 def purchase_item(item_id:str,user_id:str):
     try:
         item_find=db["item_master"].find_one({"_id":ObjectId(item_id)})
+        print(item_find)
         user_find=user_collection.find_one({"_id":ObjectId(user_id)})
         # print(item_find)
         if not item_find:
@@ -92,23 +75,49 @@ def purchase_item(item_id:str,user_id:str):
         current_balance=user_find["balance"]-purchase_item_price
         print(current_balance)
         user_collection.update_one({"_id":user_id},{"$set":{"balance":current_balance}})
+
         purchaseitem={
                        "user_id":user_find["_id"],
                        "item_id":item_find["_id"],
                        "created_at":int(datetime.timestamp(datetime.now()))
         }
         
-        user_item_collection.insert_one({"user_id":user_id,"item_id": item_id})
+        user_item_collection.insert_one({"user_id":user_id,"item_id":item_id})
         
-        existing_doc=purchase_collection.insert_one(purchaseitem)
-        return{"status_code":200,"message":"Purchase Sucessfully"}    
+        purchase_collection.insert_one(purchaseitem)
+
+        payload={
+                  "user":user_find["Name"],
+                  "item":item_find["item"],
+                  "created_at":purchaseitem["created_at"]}
+        
+        return{"status_code":200,"message":"Purchase Sucessfully","payload":payload}    
     
     except Exception as e:
-            return HTTPException(status_code=500, detail=f"Some error occured {e}")
+        return HTTPException(status_code=500, detail=f"Some error occured {e}")
+    
+@router.get("/user_purchased_item_list")
+def user_item_list(user_id:str):
+  try:
+      user=list(user_item_collection.find({"user_id":user_id}))
+    
+      if not user:
+            return HTTPException(status_code=404, detail=f"Task does not exits")
 
+      payload=[]
+      for user_item in user:
+                item=db["item_master"].find_one({"_id":ObjectId(user_item["item_id"])})
+                if item:
+                    item["_id"]=str(item["_id"]) #objectid to string
+                    payload.append(item)
+      return payload
+  except Exception as e:
+        return HTTPException(status_code=500, detail=f"Some error occured {e}")
+    
+           
 
-@router.post("/show_user_item")
-def purchase_item_list(user_id:str,item_id:str):
+@router.post("/sell_items")
+def user_sell_item(user_id:str,item_id:str):
     try:
         user=user_collection.find_one({"_id":ObjectId(user_id)})
         item=db["item_master"].find_one({"_id":ObjectId(item_id)})
@@ -128,26 +137,22 @@ def purchase_item_list(user_id:str,item_id:str):
         user_collection.update_one({"_id":user_id},{"$set":{"balance":update_balance}})
 
         user_item_collection.delete_one({"user_id": user_id, "item_id": item_id})
-        purchaseitem={
+        sellitem={
                        "user_id":user["_id"],
                        "item_id":item["_id"],
                        "created_at":int(datetime.timestamp(datetime.now()))
         }   
-        print(purchaseitem)    
-        existing_doc=sell_collection.insert_one(purchaseitem)
-        return{"status_code":200,"message": "get successfully",
-	    #  "payload":[
-		#    {
-		# 	"item_id":"6718b7667de3d1cbf7e8c459",
-		# 	"item_name":"shoes"
-		#    },
-		#    {
-		# 	"item_id":"6718b7417de3d1cbf7e8c458",
-		# 	"item_name":"chia"
-		#    }
-	    #   ]
-         }  
+        existing_doc=sell_collection.insert_one(sellitem)
+
+        payload={
+                  "user":user["Name"],
+                  "item":item["item"],
+                  "created_at":sellitem["created_at"]}
+        
+        return{"status_code":200,"message": "sell item successfully" ,"payload":payload}
+	   
     except Exception as e:
             return HTTPException(status_code=500, detail=f"Some error occured{e}")
-           
+    
+
 app.include_router(router)
